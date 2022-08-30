3a8d2000d4909fd3b02d6d739fe72686465ed5e1
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-08-30 22:56:02 +0100

    Add winning board index

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const user = 'O';
+const computer = 'X';
 // list of indexes of empty spots on the board
-const emptyIndexies = (board) => board.filter(s => s !== 'O' && s !== 'X');
-emptyIndexies();
+const avail = (reboard) => reboard.filter(s => s !== 'O' && s !== 'X');
+
+// winning combinations using the board indexes
+const winning = (board, player) => {
+  if (
+    (board[0] === player && board[1] === player && board[2] === player)
+        || (board[3] === player && board[4] === player && board[5] === player)
+        || (board[6] === player && board[7] === player && board[8] === player)
+        || (board[0] === player && board[3] === player && board[6] === player)
+        || (board[1] === player && board[4] === player && board[7] === player)
+        || (board[2] === player && board[5] === player && board[8] === player)
+        || (board[0] === player && board[4] === player && board[8] === player)
+        || (board[2] === player && board[4] === player && board[6] === player)
+  ) {
+    return true;
+  }
+  return false;
+};
+winning();
+user();
+avail();
+computer();
