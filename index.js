3840c55eb61acf5c94affc558f79bb206912983e
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-09-27 00:05:41 +0100

    Change id attribute to dataset attribute

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ const wonText = document.querySelector('.win-text');
 const replayBtn = document.querySelector('.replay');
 let element;
 
-section.addEventListener('click', () => { // once window load
-  for (let i = 0; i < allBox.length; i += 1) { // add onclick attribute to all section table
-    // eslint-disable-next-line no-use-before-define
-    allBox[i].addEventListener('click', clickedCell);
-  }
-
-  selectXBtn.addEventListener('click', () => {
-    selectBox.classList.add('hide');// hide the select box
-    playBoard.classList.add('show');// show the select button
-  });
-  selectOBtn.addEventListener('click', () => {
-    selectBox.classList.add('hide');
-    playBoard.classList.add('show');
-    players.classList.add('players', 'active', 'player');
-  });
-});
 let randomCell;
 const playerXIcon = 'fa fa-times';
 const playerOIcon = 'fa fa-circle';
@@ -42,7 +26,6 @@ const bot = (runBot) => {
     for (let i = 0; i < allBox.length; i += 1) {
       if (allBox[i].childElementCount === 0) { // if td has no any child element
         array.push(i);// inserting unselected or unclicked cells indide array section
-      // console.log(`${i}`+ '  ' + 'no child element');
       }
     }
     randomCell = array[Math.floor(Math.random() * array.length)];
@@ -52,28 +35,27 @@ const bot = (runBot) => {
         allBox[randomCell].innerHTML += `<i class="${playerXIcon}"></i>`;// adding circle icon tag inside user clicked element
         players.classList.add('active');
         playerSign = 'X';
-        allBox[randomCell].setAttribute('id', playerSign);
+        allBox[randomCell].setAttribute('data-id', playerSign);
       } else {
         allBox[randomCell].innerHTML += `<i class="${playerOIcon}"></i>`;
         players.classList.add('active');
-        allBox[randomCell].setAttribute('id', playerSign);
+        allBox[randomCell].setAttribute('data-id', playerSign);
       }
     }
     allBox[randomCell].style.pointerEvents = 'none';
     players.style.pointerEvents = 'auto';
   }
 };
-
 const selectWinner = () => {
-  const cell0 = document.querySelector('.b0').id;
-  const cell1 = document.querySelector('.b1').id;
-  const cell2 = document.querySelector('.b2').id;
-  const cell3 = document.querySelector('.b3').id;
-  const cell4 = document.querySelector('.b4').id;
-  const cell5 = document.querySelector('.b5').id;
-  const cell6 = document.querySelector('.b6').id;
-  const cell7 = document.querySelector('.b7').id;
-  const cell8 = document.querySelector('.b8').id;
+  const cell0 = document.querySelector('.b0').dataset.id;
+  const cell1 = document.querySelector('.b1').dataset.id;
+  const cell2 = document.querySelector('.b2').dataset.id;
+  const cell3 = document.querySelector('.b3').dataset.id;
+  const cell4 = document.querySelector('.b4').dataset.id;
+  const cell5 = document.querySelector('.b5').dataset.id;
+  const cell6 = document.querySelector('.b6').dataset.id;
+  const cell7 = document.querySelector('.b7').dataset.id;
+  const cell8 = document.querySelector('.b8').dataset.id;
   if ((cell0 === playerSign && cell1 === playerSign && cell2 === playerSign)
   || (cell3 === playerSign && cell4 === playerSign && cell5 === playerSign)
   || (cell6 === playerSign && cell7 === playerSign && cell8 === playerSign)
@@ -89,7 +71,7 @@ const selectWinner = () => {
     }, 700);
     // result box with winner sign
     wonText.innerHTML = `Player  <p>${playerSign}</p>  won the game`;
-  } else if (cell0 !== '' && cell1 !== '' && cell2 !== '' && cell3 !== '' && cell4 !== '' && cell5 !== '' && cell6 !== '' && cell7 !== '' && cell8 !== '') {
+  } else if (cell0 === '' && cell1 === '' && cell2 === '' && cell3 === '' && cell4 === '' && cell5 === '' && cell6 === '' && cell7 === '' && cell8 === '') {
     setTimeout(() => { // delay the show result box
       playBoard.classList.remove('show');
       resultBox.classList.add('show');
@@ -108,12 +90,12 @@ const clickedCell = (event) => {
     element.innerHTML += `<i class="${playerOIcon}"></i>`;
     players.classList.remove('active');
     playerSign = 'O';
-    element.setAttribute('id', playerSign);
+    element.setAttribute('data-id', playerSign);
   // if player select O then we'll change the playerSign value to O
   } else {
     element.innerHTML += `<i class="${playerXIcon}"></i>`;
     players.classList.remove('active');
-    element.setAttribute('id', playerSign);
+    element.setAttribute('data-id', playerSign);
   }
   selectWinner(); // calling the winner
   // disabling the cell to unclick after a click
@@ -126,12 +108,28 @@ const clickedCell = (event) => {
   }, randomDelayTime);// passing random delay time;
 };
 
+section.addEventListener('click', () => { // once window load
+  for (let i = 0; i < allBox.length; i += 1) { // add onclick attribute to all section table
+    allBox[i].addEventListener('click', clickedCell);
+  }
+
+  selectXBtn.addEventListener('click', () => {
+    selectBox.classList.add('hide');// hide the select box
+    playBoard.classList.add('show');// show the select button
+  });
+  selectOBtn.addEventListener('click', () => {
+    selectBox.classList.add('hide');
+    playBoard.classList.add('show');
+    players.classList.add('players', 'active', 'player');
+  });
+});
+
 replayBtn.addEventListener('click', () => {
   resultBox.classList.remove('show');
   playBoard.classList.add('show');
   for (let i = 0; i < allBox.length; i += 1) {
     allBox[i].innerHTML = '';
     allBox[i].style.pointerEvents = 'auto';
-    allBox[i].setAttribute('id', '');
+    allBox[i].setAttribute('data-id', '');
   }
 });
